61e2fe9f0ddebd9a07f3b565ec3fbee5c57dfbe2
male-andante <andreadanteprivitera@gmail.com>
2025-02-15 15:26:03 +0100

    inserita prima fetch

--- js/epicbooks.js
+++ js/epicbooks.js
@@ -1,4 +1,32 @@
 const baseUrl = 'https://striveschool-api.herokuapp.com/books'
 
+let allBooks = []
 
-//* 
+function getBooks() {
+    return fetch(baseUrl)
+    .then(response => response.json())
+    .then((books) => {
+        console.log(books)
+        allBooks = books
+        renderBooks(books)})
+        .catch(error => alert(error))
+}
+
+function renderBooks(books){
+
+
+    const listSection = document.getElementById('listSection')
+    const listCol = document.createElement('div')
+    listCol.classList.add('col-6', 'col-md-4', 'col-lg-3')
+    listSection.appendChild(listCol)
+
+    const bookCard  = document.createElement('div')
+    bookCard.classList.add('card')
+    const bookCover  = document.createElement('img')
+    bookCover
+
+
+    
+}
+
+getBooks()
